20046ac66807d5e1caaf6c66d50f6ec622255087
noureddineouafy <130229691+noureddineouafy@users.noreply.github.com>
2023-07-26 14:14:34 +0100

    Delete plugins directory

--- plugins/mipilot-serbot-info.js
+++
@@ -1,7 +0,0 @@
-async function handler(m, { usedPrefix }) {
-let users = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user)])]
-await m.reply( '*—◉ instagram.com/noureddine_ouafy*')
-await m.reply(users.map(v => '👉🏻 wa.me/' + v.jid.replace(/[^0-9]/g, '') + `?text=${usedPrefix}estado (${v.name})`).join('\n'))}
-handler.command = handler.help = ['listjadibot','bots','subsbots']
-handler.tags = ['jadibot']
-export default handler
