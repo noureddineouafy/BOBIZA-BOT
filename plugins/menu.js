a1ac57f1b272a27b90443976f876d7074eb9d0e0
noureddineouafy <130229691+noureddineouafy@users.noreply.github.com>
2023-07-26 18:42:46 +0100

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,38 +1,182 @@
 let handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems })  => m.reply(`
+🌟قسم الذكاء الاصطناعي
+╔════▣◎▣════╗ 
+.gptanime
+.openai
+.chatgpt
+.chatgpt1
+.imagin
+.ava
+.chatgpt
+.lexica
+.dirlia
+.draw
+.gpt4
+.ava
+.ai
+.go
+.ai
+.dx
+.ai2
+╚════▣◎▣════╝
 
-▔▔▔▔▔▔▔◥ 🧡 ◤▔▔▔▔▔▔▔
-🎲 .translate
-🎲 .tr
-🎲 .sticker
-🎲 .wm
-🎲 .totexte
-🎲 .attp
-🎲 .textpdf
+ 🍁  قسم الأنيم
+╔════▣◎▣════╗
+.animepic
+.tofanime
+.animeai
+.tofanime
+.storyanime
+.toanim
+╚════▣◎▣════╝
 
-•——————•°•✿•°•——————•
-*قسم الذكاء الإصطناعي*
+📰 قسم الأخبار 
+╔════▣◎▣════╗
+.hespress
+.technews
+.whabetainfo
 
-💻   .chatgpt
-💻   .gpt4
-💻   .ai
-💻   .ai2
-💻   .go
-💻   .dx
-💻   .dirlia
-💻   .draw
-•——————•°•✿•°•——————•
-الدين هو الاسلام : 
-🕋 .ayati
-🕋 .khawatir
-🕋 .bader
-🕋 .aya
+╚════▣◎▣════╝
 
-البوت يتضمن ازيد من 100 أمر أغلبيتها تم إخفاؤها من طرف نورالدين 😄😉
+📹 قسم الفيديوات
+╔════▣◎▣════╗
+.khawatir
+.reels
+.stories
+.bader
 
-▔▔▔▔▔▔▔◥ 🧡 ◤▔▔▔▔▔▔▔ 
-إنضم الى مجموعة بوبيزة بوت \n https://chat.whatsapp.com/FCudAHwTYLtJu242Zv1MmR\n
-يمكنك زيارة أنستغرامي لتتعرف أكثر على خدمات البوت وايضا لتتعرف على صاحب البوت \n
-*instagram :*\ninstagram.com/noureddine_ouafy \n*youtube :*\nhttps://youtube.com/@noureddineouafy2 \n*facebook:*\nhttps://www.facebook.com/profile.php?id=100063533185520 \n`.trim())
+╚════▣◎▣════╝
+   
+🔰 قسم التطبيقات 
+╔════▣◎▣════╗
+.apk4
+.apkmirror
+.bobizaapk
+.apkpuresearch
+.apps
+.mod
+.mapk
+╚════▣◎▣════╝
+
+📚 قسم الكتب 
+╔════▣◎▣════╗
+.alloschool
+.kitaby
+.bookspdf
+╚════▣◎▣════╝
+
+🌠 قسم الملصقات 
+╔════▣◎▣════╗
+.s
+.qc
+.attp
+.sticker
+.smeme
+.wm
+.take
+.nour
+.quotly
+.quotlyv2
+.img
+.tovideo
+.quotlyv3
+╚════▣◎▣════╝
+
+ 📷 قسم الصور
+╔════▣◎▣════╗
+.wallpaper
+.hdr
+.dehaz
+.recoler
+.cartoon
+.remini
+.remini2
+.animeai
+.bobizalogo
+.styletexte
+.quotlyimg
+.quotlyimgv2
+.quotlyimgv3
+.hd
+.pdf
+.removebg
+.rg
+.hanan
+.tweet
+.texte
+╚════▣◎▣════╝
+
+
+🔊 قسم التحميلات 
+╔════▣◎▣════╗
+.ig v1
+.ig v2
+.instagram
+.tiktok
+.mp3
+.songs
+.dlmp3
+.savefrom
+.
+╚════▣◎▣════╝
+
+🕋 الدين هو الإسلام 
+╔════▣◎▣════╗
+.coran
+.aya
+.ramadan
+.ayatkorsi
+.khawatir
+╚════▣◎▣════╝
+
+قسم البحث🔎
+╔════▣◎▣════╗
+.google
+.wiki
+.yts
+.groupwa
+.udemy
+╚════▣◎▣════╝
+TOOLS ♥
+╔════▣◎▣════╗
+.dns
+.dafonts
+.tomp3
+.fakechat
+.fakeemail
+.getprofile
+.instauser
+.bruzu
+.carbon
+.jadibot
+.maps
+.audio
+.runtime
+.bot
+.ibb
+.enc
+.ping
+.climate
+.speedtest
+.wbi
+.telegraph
+.truecaller
+.trucaller2
+.toanim
+.tomp3
+.textpdf
+.translate
+.tr
+╚════▣◎▣════╝
+*instagram :*\n
+instagram.com/noureddine_ouafy \n
+*youtube :*\n
+https://youtube.com/@noureddineouafy2 \n
+*facebook:*\n
+https://www.facebook.com/profile.php?id=100063533185520 \n
+
+
+`.trim())
 handler.help = ['allmenu']
 handler.tags = ['main']
 handler.command = ['list', 'menu'] 
